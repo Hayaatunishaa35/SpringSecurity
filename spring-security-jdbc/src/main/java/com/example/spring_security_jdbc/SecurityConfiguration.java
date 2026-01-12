ebd4e8805f89c821cde94bab9cd5db0006527d03
Hayaatunishaa35 <hayatunisha1@gmail.com>
2026-01-12 21:35:14 +0530

    hardcoded cred

--- spring-security-jdbc/src/main/java/com/example/spring_security_jdbc/SecurityConfiguration.java
+++ spring-security-jdbc/src/main/java/com/example/spring_security_jdbc/SecurityConfiguration.java
@@ -1,11 +1,60 @@
 package com.example.spring_security_jdbc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/user").hasAnyRole("ADMIN","USER")
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()).formLogin(form -> {});
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder, DataSource dataSource){
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+        if(!manager.userExists("user")){
+            manager.createUser(
+                    User.withUsername("user")
+                            .password(encoder.encode("pass"))
+                            .roles("USER")
+                            .build()
+            );
+        }
+
+        if(!manager.userExists("admin")){
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password(encoder.encode("pass"))
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }

9b08eb834cad0e69977336414342dbd9a68236a1
Hayaatunishaa35 <hayatunisha1@gmail.com>
2026-01-15 00:54:05 +0530

    Custom UserDetails/Service

--- spring-security-jpa/src/main/java/com/example/spring_security_jpa/SecurityConfiguration.java
+++ spring-security-jpa/src/main/java/com/example/spring_security_jpa/SecurityConfiguration.java
@@ -1,15 +1,30 @@
 package com.example.spring_security_jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin").hasRole("ROLE_ADMIN")
+                .requestMatchers("/user").hasAnyRole("ROLE_ADMIN","ROLE_USER")
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()).formLogin(form -> {});
+
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();

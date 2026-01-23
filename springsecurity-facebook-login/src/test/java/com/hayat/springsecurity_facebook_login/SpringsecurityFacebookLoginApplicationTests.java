75cb086947675eba474fe504d57de9400f2e9c1a
Hayaatunishaa35 <hayatunisha1@gmail.com>
2026-01-23 15:03:45 +0530

    OAuth authencation through facebook

---
+++ springsecurity-facebook-login/src/test/java/com/hayat/springsecurity_facebook_login/SpringsecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayat.springsecurity_facebook_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
